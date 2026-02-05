649117eb647da26cf792c57b27f308b952c712db
cristophermoreno <cristophermoreno@voxbit.net>
2026-02-05 13:52:40 +0000

    --

--- lua/plugins/templ.lua
+++ lua/plugins/templ.lua
@@ -1,4 +1,14 @@
 return {
-  "joerdav/templ.vim",
-  ft = { "templ" },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    keys = {
+      { "<leader>t", "<cmd>ToggleTerm<cr>", desc = "Terminal" },
+    },
+    config = function()
+      require("toggleterm").setup({
+        shade_terminals = true,
+      })
+    end,
+  },
 }

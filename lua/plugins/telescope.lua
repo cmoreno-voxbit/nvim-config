cd854c024d10951c5955eaaeaf8724390fd31bed
Cristopher <morenopalmer99@gmail.com>
2026-04-09 22:48:27 +0100

    --

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require("telescope").setup({
       defaults = {
-        file_ignore_patterns = { "node_modules", ".git/" },
+        file_ignore_patterns = { "node_modules", ".git/", ".env/",".venv/" },
         layout_strategy = "flex",
       },
       pickers = {

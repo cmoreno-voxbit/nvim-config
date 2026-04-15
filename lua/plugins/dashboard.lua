78a451afe7632a7378db9d0310465fc77cead479
cristophermoreno <cristophermoreno@voxbit.net>
2026-04-15 08:36:39 +0000

    --

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,9 +1,15 @@
 return {
   {
     "goolord/alpha-nvim",
+    lazy = false, 
+    priority = 1000,
     opts = function(_, dashboard)
-      local logo = [[NVIM]]
+      local logo = [[ NVIM ]]
       dashboard.section.header.val = vim.split(logo, "\n")
+      return dashboard
+    end,
+    config = function(_, opts)
+      require("alpha").setup(opts)
     end,
   },
 }
